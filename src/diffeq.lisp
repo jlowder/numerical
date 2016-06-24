854becdfe33db30c23bae999e35a44d6bd74388b
Jason Lowdermilk <jlowdermilk@rtlogic.com>
2016-06-24 16:55:08 -0600

    started extrapolated-midpoint/4

--- src/diffeq.lisp
+++ src/diffeq.lisp
@@ -59,3 +59,20 @@
          as yi = (+ y (* 1d0 step m))
          finally (return yi)))))
 
+(defun extrapolated-midpoint/4 (fxy)
+  (labels ((extrapolate (l &optional (ni 2) nl)
+             (if (cadr l)
+                 (let ((num (expt 2 ni)))
+                   (extrapolate (cdr l) ni (append nl (list (/ (- (* num (cadr l)) (car l)) (1- num))))))
+                 (if nl (extrapolate nl (+ ni 2)) (car l)))))
+    (lambda (a b &key (initial 0d0) (n 1))
+      (extrapolate (loop repeat 4
+                      as m = 2 then (+ m m)
+                      as step = (/ (- b a) (* m n))
+                      collect (loop repeat (* n m)
+                                 as y = initial then yi
+                                 as xi = a then (+ xi step)
+                                 as f0 = (funcall fxy xi y)
+                                 as m = (* 1/2 (+ f0 (funcall fxy (+ xi step) (+ y (* step f0)))))
+                                 as yi = (+ y (* 1d0 step m))
+                                 finally (return yi)))))))
